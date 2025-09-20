368122852a351dfec852ca08cc079ece523cff8a
nguyenlean17 <nguyenlean17@gmail.com>
2025-09-20 22:15:09 +0700

    Bai tap tuan 1

---
+++ HW1/Introduction.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Introduction {
+    public static void main(String[] args) {
+
+        System.out.println("Nguyễn Lê An\t24020004\tK69I-IT4\tnguyenlean17\tnguyenlean17@gmail.com");
+        for (int i = 9; i >= 1; i--) {
+            System.out.println(i+" bottles of beer on the wall,"+ i+" bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles on the wall.");
+    }
+}
